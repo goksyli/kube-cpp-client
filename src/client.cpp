c6c21dc92a9b471a76a945fbba7c8b4dee049f04
goksyli <goksyli1990@gmail.com>
2015-11-13 21:10:07 +0800

    add client.cpp and cmake sucessfully

--- src/client.cpp
+++ src/client.cpp
@@ -2,10 +2,10 @@
 #include <json.hpp>
 #include <iostream>
 using Http = asoni::Handle;
-
+using json = nlohmann::json;
 int main(int argc, char **argv)
 {
-    auto res1 = Http().get("http://127.0.0.1:8080/api/v1/pods")
+    auto result = Http().get("http://127.0.0.1:8080/api/v1/pods")
         .exec();
 
     auto response = json::parse(result.body);
